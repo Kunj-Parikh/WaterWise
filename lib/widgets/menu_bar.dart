14097e9573829c2abeeca0d2cac2f080e505b2b9
Kunj Parikh <9002birdwatcher@gmail.com>
2025-06-03 14:20:07 -0400

    menu bar integrated but not finished

---
+++ lib/widgets/menu_bar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FilterMenuBar extends StatefulWidget {
+  const FilterMenuBar({super.key});
+
+  @override
+  State<FilterMenuBar> createState() => _FilterMenuBarState();
+}
+
+class _FilterMenuBarState extends State<FilterMenuBar> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Hello world')
+    );
+  }
+}
