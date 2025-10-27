702251a6d8319dac7c169dc4c6c9a6bfa6bb093c
giannap27 <gayoungp06@gmail.com>
2025-10-27 00:23:59 -0400

    welcome page

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -8,12 +8,14 @@ class Welcome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Positioned.fill(
+        FractionallySizedBox(
+          widthFactor: 0.7,
+          heightFactor: 0.5,
           child: Container(
             color: Colors.black54,
             child: Center(
               child: Container(
-                width: 400,
+                margin: EdgeInsets.symmetric(horizontal: 50, vertical: 24),
                 padding: const EdgeInsets.all(24),
                 decoration: BoxDecoration(
                   color: Colors.white,
@@ -37,8 +39,9 @@ class Welcome extends StatelessWidget {
                           const Text(
                             'Find out about nearby water quality data of PFOA, lead, nitrate, and aresnic. Start by clicking "My Location "',
                             textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.black, fontSize: 22),
                           ),
-                          const SizedBox(height: 20),
+                          const SizedBox(height: 50),
                           ElevatedButton(
                             onPressed: onClose,
                             style: ElevatedButton.styleFrom(
@@ -50,9 +53,9 @@ class Welcome extends StatelessWidget {
                               ),
                               foregroundColor: const Color.fromARGB(
                                 255,
-                                220,
-                                74,
-                                74,
+                                0,
+                                0,
+                                0,
                               ),
                             ),
                             child: const Text('Go.'),
